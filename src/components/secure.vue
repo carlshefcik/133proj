0dda5693da849f1ef34ba4a0a65f92e3ff49e199
carlshefcik <32783344+carlshefcik@users.noreply.github.com>
2018-08-22 21:02:45 -0700

    changed made some pages

---
+++ src/components/secure.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Secure Page</h1>
+    <div>
+      <router-link to="/">Go back</router-link>
+    </div>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/home'
+import Secure from '@/components/secure'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/secure',
+      name: 'Secure',
+      component: Secure
     }
   ]
 })
