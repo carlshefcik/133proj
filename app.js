e9851c9545ab65d34b95afe87753071d435a643a
carlshefcik <32783344+carlshefcik@users.noreply.github.com>
2018-09-04 01:48:45 -0700

    Worked on express

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 var express = require('express');
-var app = express();
 var bodyParser = require ('body-parser');
-app.use(bodyParser.urlencoded({extended: true})); //hook up with your app
-app.use(express.static('./')); //initializing the app with the directory of the app.js
+var path = require('path');
+
+var app = express();
+
+// View engine
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+
+// Body-parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false})); //hook up with your app
+
+// Set static path
+app.use(express.static(path.join(__dirname, 'src'))); //initializing the app with the directory of the app.js
 
 
-app.listen(8000, () => {
-    console.log('server started at http://localhost:8000/src/index.html') //boots up node.js server
+app.get('/', (req, res)=>{
+    res.send('Hello')
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`server started at http://localhost:${port}/`) //boots up node.js server
 });
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,8 @@
-console.log(document)
+$(document).ready(() => {
+    console.log('index loaded');
+
+    document.getElementById("login-button").addEventListener('click', function(e){
+        console.log('clicked');
+        document.getElementById('status').innerHTML = "Hello, the button only displays this.";
+    });
+});
