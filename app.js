a4f8efa2cf4e280fc5bd6c5c6502c229ca92a58f
Jackbui96 <nguyenbui1996@gmail.com>
2018-09-05 13:14:11 -0700

    test for firebase connection

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var bodyParser = require ('body-parser');
 var path = require('path');
+var firebase = require("firebase-admin");
 
 var app = express();
+var serivceAccount = require("./serviceAccountKey.json");
+
+firebase.initializeApp({
+    credential: firebase.credential.cert(serivceAccount),
+    databaseURL: "https://testing-island.firebaseio.com"
+});
+
+var db = firebase.database();
+var ref = db.ref("restricted_access/secret_document");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
+
+var usersRef = ref.child("users");
+usersRef.set({
+  alanisawesome: {
+    date_of_birth: "June 23, 1912",
+    full_name: "Alan Turing"
+  },
+  gracehop: {
+    date_of_birth: "December 9, 1906",
+    full_name: "Grace Hopper"
+  }
+});
 
 // Body-parser middleware
 app.use(bodyParser.json());
@@ -26,7 +51,7 @@ app.get('/', (req, res)=>{
 });
 
 app.get('/get_sales', (req,res)=>{
-    res.send(sale);
+    res.send(usersRef);
 });
 
 const port = process.env.PORT || 8000;
