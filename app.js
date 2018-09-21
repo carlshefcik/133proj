eebf4027f25abf1693b79c422a9ff0ad3f3f2f44
NguyenBui96 <nguyenbui1996@gmail.com>
2018-09-20 20:58:44 -0700

    forgot to add @ sign.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require ('body-parser');
 const path = require('path');
 const firebase = require('firebase');
 const serviceAccount = require("./serviceAccountKey.json");
-const emailRegex = new RegExp(/@gmail.com|@yahoo.com|@hotmail.com|@icloud.com|aol.com|msn.com/);
+const emailRegex = new RegExp(/@gmail.com|@yahoo.com|@hotmail.com|@icloud.com|@aol.com|@msn.com/);
 
 var app = express();
 
@@ -30,22 +30,8 @@ app.use(bodyParser.urlencoded({extended: false})); //hook up with your app
 // Set static path
 app.use(express.static(path.join(__dirname, 'src'))); //initializing the app with the directory of the app.js
 
-const sale = [
-    {
-        item: 'Bannanas',
-        percent: 30
-    },{
-        item: 'Ice Cream',
-        percent: 50
-    }
-];
-
 app.get('/', (req, res)=>{
     res.send('Hello');
-});
-
-app.get('/get_sales', (req,res)=>{
-    res.send(sale);
 });
 
 app.post('/register_user', (req,res)=>{
@@ -68,13 +54,6 @@ app.post('/register_user', (req,res)=>{
         alert('Either email nor password is invalid.');
         return;
     }
-
-
-    //var link = db.collection("Customer").doc();
-    //var insert = link.set({
-    //    name: email,
-    //    password: password
-    //});
     
 });
 
