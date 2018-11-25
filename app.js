b71891362e5fbf00993d0c5932fd2feefe706792
stinaan <38736858+stinaan@users.noreply.github.com>
2018-11-25 13:55:02 -0800

    Merge branch 'master' of https://github.com/carlshefcik/133proj

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ const emailRegex = new RegExp(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/
 var app = express();
 
 var config = {
-    apiKey: "AIzaSyDS9owx0q_Eq96Cs2T-xD0s_cEHi4AxrEI",
-    authDomain: "testing-island.firebaseapp.com",
-    databaseURL: "https://testing-island.firebaseio.com",
-    projectId: "testing-island",
-    storageBucket: "testing-island.appspot.com",
-    messagingSenderId: "1059701996270"
+  apiKey: "AIzaSyDS9owx0q_Eq96Cs2T-xD0s_cEHi4AxrEI",
+  authDomain: "testing-island.firebaseapp.com",
+  databaseURL: "https://testing-island.firebaseio.com",
+  projectId: "testing-island",
+  storageBucket: "testing-island.appspot.com",
+  messagingSenderId: "1059701996270"
 };
 const firebaseService = firebase.initializeApp(config);
 
@@ -28,6 +28,7 @@ var firebaseStorage = firebase.storage()
 var firebaseFirestore = firebaseService.firestore();
 const settings = { timestampsInSnapshots: true };
 firebaseFirestore.settings(settings);
+
 
 // var firebaseMessaging = firebaseService.messaging(); This is be use later
 
@@ -39,104 +40,98 @@ app.use(bodyParser.urlencoded({ extended: false })); //hook up with your app
 app.use(express.static(path.join(__dirname, 'public'))); //initializing the app with the directory of the app.js
 
 app.get('/', (req, res) => {
-    res.send('Hello');
+  res.send('Hello');
 });
 
 //returns a json of element names of the aisles and the groups as corresponding arrays
 app.get('/aisles_and_groups', (req, res) => {
-    let items = {}
-    firebaseFirestore.collection("Aisles").get().then((coll) => {
-        coll.forEach((doc) => {
-            var tmp = doc.id;
-            items[tmp] = [];
-            // adds groups to the aisles element arrays
-            doc.data()['subCollections'].forEach((e) => {
-                items[tmp].push(e);
-            })
-        });
-        res.send(items);
-    }).catch(function (error) {
-        console.log(error);
+  let items = {}
+  firebaseFirestore.collection("Aisles").get().then((coll) => {
+    coll.forEach((doc) => {
+      var tmp = doc.id;
+      items[tmp] = [];
+      // adds groups to the aisles element arrays
+      doc.data()['subCollections'].forEach((e) => {
+        items[tmp].push(e);
+      })
     });
+    res.send(items);
+  }).catch(function (error) {
+    console.log(error);
+  });
 })
 
 //gets the aisles and their img urls
 app.get('/load_aisles', (req, res) => {
-    let aisles = {}
-    firebaseFirestore.collection("Aisles").get().then((coll) => {
-        coll.forEach((doc) => {
-            let tmp = doc.id
-            aisles[tmp] = []
-            aisles[tmp].push(tmp)
-            aisles[tmp].push(doc.data()['imgURL'])
-        })
-        res.send(aisles)
-    }).catch(function (error) {
-        console.log(error)
+  let aisles = {}
+  firebaseFirestore.collection("Aisles").get().then((coll) => {
+    coll.forEach((doc) => {
+      let tmp = doc.id
+      aisles[tmp] = []
+      aisles[tmp].push(tmp)
+      aisles[tmp].push(doc.data()['imgURL'])
     })
+    res.send(aisles)
+  }).catch(function (error) {
+    console.log(error)
+  })
 })
 
 //loads all items from requested aisle
 app.get('/load_items', (req,res) => {
-    let items = []
-    let aisleName = Object.keys(req.query)[0]
-    let groups
-    console.log(aisleName)
+  let items = []
+  let aisleName = Object.keys(req.query)[0]
+  let groups
+  console.log(aisleName)
 
-    getItems()
+  getItems()
 
-    function getItems() {
-        firebaseFirestore.collection("Aisles").doc(aisleName).get().then((doc) =>{
-            groups = doc.data()["subCollections"]
-        }).then((data) =>{
-            groups.forEach((groupName) =>{
-                firebaseFirestore.collection("Aisles").doc(aisleName).collection(groupName).get().then((coll) =>{
-                    coll.forEach((doc) =>{
-                        // put in array and then add to array
-                        items.push([doc.data()["name"], doc.id, doc.data()["imgURL"]])
-                    })
-                })
-            })
+  function getItems() {
+    firebaseFirestore.collection("Aisles").doc(aisleName).get().then((doc) =>{
+      groups = doc.data()["subCollections"]
+    }).then((data) =>{
+      groups.forEach((groupName) =>{
+        firebaseFirestore.collection("Aisles").doc(aisleName).collection(groupName).get().then((coll) =>{
+          coll.forEach((doc) =>{
+            // put in array and then add to array
+            items.push([doc.data()["name"], doc.id, doc.data()["imgURL"]])
+          })
         })
-        // for some reason firebase does an async call on firestore functions
-        setTimeout(function(){res.send(items)}, 2000)
-    }
+      })
+    })
+    // for some reason firebase does an async call on firestore functions
+    setTimeout(function(){res.send(items)}, 2000)
+  }
 })
 
 //loads items for item page
 app.get('/load_item_info', (req,res) => {
-    let itemInfo = {}
-    let itemId = Object.keys(req.query)[0]
-    console.log(itemId)
+  let itemInfo = {}
+  let itemId = Object.keys(req.query)[0]
+  console.log(itemId)
 
-    firebaseFirestore.collection("Items").doc(itemId).get().then((doc) =>{
-        if (doc.exists) {
-            // sets doc data to the item info
-            itemInfo = doc.data()
-            res.send(itemInfo)
-        } else {
-            console.log("Item doesnt exist")
-            res.redirect(404, '/404.html')
-        }
-    })
+  firebaseFirestore.collection("Items").doc(itemId).get().then((doc) =>{
+    if (doc.exists) {
+      // sets doc data to the item info
+      itemInfo = doc.data()
+      res.send(itemInfo)
+    } else {
+      console.log("Item doesnt exist")
+      res.redirect(404, '/404.html')
+    }
+  })
 })
 
 app.get('/get_popular', (req,res)=>{
-    let popular = {
-        "test1": ["id1", "name1", 1, "image1"],
-        "test3": ["id3", "name3", 2, "image3"],
-        "test2": ["id2", "name2", 3, "image2"],
-        "test4": ["id4", "name4", 4, "image4"],
-        "test5": ["id5", "name5", 5, "image5"]
-    }
-    res.send(popular)
+  let popular = {
+    "test1": ["id1", "name1", 1, "image1"],
+    "test3": ["id3", "name3", 2, "image3"],
+    "test2": ["id2", "name2", 3, "image2"],
+    "test4": ["id4", "name4", 4, "image4"],
+    "test5": ["id5", "name5", 5, "image5"]
+  }
+  res.send(popular)
 })
-
-
-
-
-
-
 
 //////////////////////////////////////////////
 //adds viewed items to array
@@ -148,16 +143,9 @@ app.get('/get_popular', (req,res)=>{
 
 //}
 
-
-
-
-
-
-
 //adds a new item to the database
 app.post('/add_item', (req, res) => {
     //verifies that the user is an admin
-
 
     let itemData = req.body
     // console.log(itemData)
@@ -182,210 +170,276 @@ app.post('/add_item', (req, res) => {
         firebaseFirestore.collection('Aisles').doc(itemData.Aisle).collection(itemData.Group).doc(itemid).set(itemProperties)
         res.send(itemid)
     });
-
-    // Image upload snippett
-    //{
-    // console.log("uploading image")
-    // // Create file metadata including the content type
-    // var metadata = { contentType: 'image/jpeg' };
-    // // Upload the file and metadata
-    // var uploadTask = firebaseStorage.ref().child("Aisles/Bakery/Bread/default.jpg").put(itemData.Image, metadata)
-
-    // // Register three observers:
-    // // 1. 'state_changed' observer, called any time the state changes
-    // // 2. Error observer, called on failure
-    // // 3. Completion observer, called on successful completion
-    // uploadTask.on('state_changed', function(snapshot){
-    //     // Observe state change events such as progress, pause, and resume
-    //     // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-    //     var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    //     console.log('Upload is ' + progress + '% done');
-    //     switch (snapshot.state) {
-    //     case firebase.storage.TaskState.PAUSED: // or 'paused'
-    //         console.log('Upload is paused');
-    //         break;
-    //     case firebase.storage.TaskState.RUNNING: // or 'running'
-    //         console.log('Upload is running');
-    //         break;
-    //     }
-    // }, function(error) {
-    //     // Handle unsuccessful uploads
-    //     console.log("didnt upload")
-    // }, function() {
-    //     // Handle successful uploads on complete
-    //     // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-    //     uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-    //         console.log('File available at', downloadURL);
-    //         itemProperties.imgURL = downloadURL
-    //     });
-    // });
-    //}
-
-    // //add imgURL to original item document
-    // firebaseFirestore.collection('Items').doc(itemid).set({
-    //     imgURL: itemProperties.imgURL
-    // }).then(ref => {
-    //     console.log('Added changed imgurl of: ', ref.id)
-    // })
 })
 
 app.post('/register_user', (req, res) => {
-    console.log('going')
-    let email = req.body.username;
-    let password = req.body.password;
-    let userID;
+  console.log('going')
+  let email = req.body.username;
+  let password = req.body.password;
+  let userID;
 
-    if (emailRegex.test(email) && password.length >= 6) {
-      firebaseAuth.createUserWithEmailAndPassword(email, password)
-      .then(function(data){
-        console.log("Test here: " + data.user.uid);
-        firebaseFirestore.collection("Customer").doc(data.user.uid).set({
-          email: email,
-          lastName: "",
-          firstName: ""
-        })
-        console.log("success");
-        res.send(email);
-      }).catch(function(error){
-        console.log("fail");
+  if (emailRegex.test(email) && password.length >= 6) {
+    firebaseAuth.createUserWithEmailAndPassword(email, password)
+    .then(function(data){
+      console.log("Test here: " + data.user.uid);
+      firebaseFirestore.collection("Customer").doc(data.user.uid).set({
+        email: email,
+        lastName: "",
+        firstName: ""
       })
-    }
+      console.log("success");
+      res.send(email);
+    }).catch(function(error){
+      console.log("fail");
+    })
+  }
 
 });
 
 app.post('/login_user', (req, res) => {
 
-    console.log(req.body);
-    let email = req.body.username;
-    console.log("here is email: " + email);
-    let password = req.body.password;
-    console.log("here is password: " + password);
+  console.log(req.body);
+  let email = req.body.username;
+  console.log("here is email: " + email);
+  let password = req.body.password;
+  console.log("here is password: " + password);
 
-    let user1 = ""
-    let promise1 = new Promise(function (resolve, reject) {
-        firebaseAuth.onAuthStateChanged(function (user) {
-            user1 = user
-        })
-        resolve('data')
-    });
+  let user1 = ""
+  let promise1 = new Promise(function (resolve, reject) {
+    firebaseAuth.onAuthStateChanged(function (user) {
+      user1 = user
+    })
+    resolve('data')
+  });
 
-    promise1.then(function (data) {
-        if (user1) {
-            console.log("User already logged in!!!")
-        } else {
-            if (emailRegex.test(email) && password.length >= 6) {
-                firebaseAuth.signInWithEmailAndPassword(email, password);
-                res.send(email);
-                console.log("login successed");
-            } else {
-                console.log("login failed");
-                //alert('Either email nor password is invalid.');
-                return;
-            }
-        }
-    });
+  promise1.then(function (data) {
+    if (user1) {
+      console.log("User already logged in!!!")
+    } else {
+      if (emailRegex.test(email) && password.length >= 6) {
+        firebaseAuth.signInWithEmailAndPassword(email, password);
+        res.send(email);
+        console.log("login successed");
+      } else {
+        console.log("login failed");
+        //alert('Either email nor password is invalid.');
+        return;
+      }
+    }
+  });
 });
 
 app.get('/logOutUser', (req, res) => {
 
-    let user1 = ""
-    let promise1 = new Promise(function (resolve, reject) {
-        firebaseAuth.onAuthStateChanged(function (user) {
-            user1 = user
-        })
-        resolve('data')
+  let user1 = ""
+  let promise1 = new Promise(function (resolve, reject) {
+    firebaseAuth.onAuthStateChanged(function (user) {
+      user1 = user
     })
+    resolve('data')
+  })
 
-    promise1.then(function (data) {
-        if (user1) {
-            firebaseAuth.signOut();
-            console.log("Logged out succesful")
-            res.send("Logged out succesfull")
-            //res.send("Guest")
-        }
-    })
+  promise1.then(function (data) {
+    if (user1) {
+      firebaseAuth.signOut();
+      console.log("Logged out succesful")
+      res.send("Logged out succesfull")
+      //res.send("Guest")
+    }
+  })
 });
 
 app.get('/check_user_status', (req, res) => {
-    console.log("I am over here!!!!")
-    let user1 = ""
-    let promise1 = new Promise(function (resolve, reject) {
-        firebaseAuth.onAuthStateChanged(function (user) {
-            user1 = user
-        })
-        resolve('data')
+  console.log("I am over here!!!!")
+  let user1 = ""
+  let promise1 = new Promise(function (resolve, reject) {
+    firebaseAuth.onAuthStateChanged(function (user) {
+      user1 = user
     })
+    resolve('data')
+  })
 
-    promise1.then(function (data) {
-        if (user1) {
-            console.log("User is logged in!!!!")
-            res.send(user1.email)
-        } else {
-            console.log("User is not logged in!!!!")
-            res.send("Guest")
-        }
+  promise1.then(function (data) {
+    if (user1) {
+      console.log("User is logged in!!!!")
+      res.send(user1.email)
+    } else {
+      console.log("User is not logged in!!!!")
+      res.send("Guest")
+    }
+  })
+
+});
+
+app.get('/check_user_status_1', (req, res) => {
+  console.log("I am over here!!!!")
+  let user1 = ""
+  let promise1 = new Promise(function (resolve, reject) {
+    firebaseAuth.onAuthStateChanged(function (user) {
+      user1 = user
     })
+    resolve('data')
+  })
+
+  promise1.then(function (data) {
+    if (user1) {
+      console.log("User is logged in!!!!")
+      res.send(user1.uid)
+    } else {
+      console.log("User is not logged in!!!!")
+      res.send("Guest")
+    }
+  })
+
+});
+
+app.post('/send_reset_email', (req, res) => {
+
+  let email = req.body.emailReset;
+  console.log(email);
+
+  firebaseAuth.sendPasswordResetEmail(email)
 
 });
 
 app.get('/get_cart', (req, res) => {
-    console.log("test");
-    var ref = firebaseFirestore.collection('Aisles').doc('Bakery').collection('Bread');
-    ref.get()
-        .then((snapshot) => {
-            snapshot.forEach((doc) => {
-                console.log(doc.id, '=>', doc.data());
-            });
-        })
-        .catch((err) => {
-            console.log('Error getting documents', err);
-        });
+  console.log("test");
+  var ref = firebaseFirestore.collection('Aisles').doc('Bakery').collection('Bread');
+  ref.get()
+  .then((snapshot) => {
+    snapshot.forEach((doc) => {
+      console.log(doc.id, '=>', doc.data());
+    });
+  })
+  .catch((err) => {
+    console.log('Error getting documents', err);
+  });
 });
+
+//adds given item id to the cart
+app.get('/add_to_cart', (req, res) =>{
+    //pareses data
+    let data = Object.keys(req.query)[0]
+    let itemID = req.query[data][0]
+    let itemQuantity = req.query[data][1]
+
+    // variable for the user
+    var firebaseUser = firebaseAuth.currentUser;
+
+    if(firebaseUser){ //if there is a user logged in
+        let userID = firebaseUser.uid
+        let userDB = firebaseFirestore.collection("Customer").doc(userID)
+        let cart;
+
+        //query for email
+        firebaseFirestore.collection("Customer").doc(userID).get().then((doc) =>{
+            if (doc.exists) {
+                // gets cart reference in the db
+                cart = doc.get("cart") ? doc.get("cart") : {};
+
+                if(cart[itemID]){
+                    //adds more items if the item is alread in the cart
+                    cart[itemID] += parseInt(itemQuantity, 10)
+                } else {
+                    // creates a new map element in the cart if item is not there
+                    Object.defineProperty(cart, itemID, {
+                        value: parseInt(itemQuantity, 10),
+                        writable: true,
+                        enumerable: true,
+                        configurable: true
+                    })
+                }
+
+                //updates the cart with the new cart data
+                userDB.update({
+                    cart: cart
+                })
+
+                res.send("sucess")
+            }
+        })
+
+    } else { //no user logged in so must store cart in cache
+        res.status(304).send("No user logged in, store in cache")
+    }
+})
 
 app.get('/get_inventory', (req, res) => {
 
-    let items = [];
-    //var ref_inventory = firebaseFirestore.collection('Aisles').doc("Bakery/Bread");
+  let items = [];
+  //var ref_inventory = firebaseFirestore.collection('Aisles').doc("Bakery/Bread");
 
-    firebaseFirestore.collection("Aisles").get().then(function (querySnapshot) {
-        querySnapshot.forEach(function (doc) {
-            var tmp = doc.id;
-            items.push(tmp);
-            //console.log(items);
+  firebaseFirestore.collection("Aisles").get().then(function (querySnapshot) {
+    querySnapshot.forEach(function (doc) {
+      var tmp = doc.id;
+      items.push(tmp);
+      //console.log(items);
 
-        });
-        console.log(items);
-        //send items before error handling
-        res.send(items);
-    }).catch(function (error) {
-        console.log(error);
     });
+    console.log(items);
+    //send items before error handling
+    res.send(items);
+  }).catch(function (error) {
+    console.log(error);
+  });
 });
 
 app.get('/get_sales', (req,res)=>{
 
-    let sales = [];
+  let sales = [];
 
-    firebaseFirestore.collection("Items").get().then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
+  firebaseFirestore.collection("Items").get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
 
-            if (doc.get("sale") == true) {
-                sales.push(doc.get("name"));
-            }
-        });
-        console.log(sales);
-        //send items before error handling
-        res.send(sales);
-    }).catch(function(error){
-        console.log(error);
+      if (doc.get("sale") == true) {
+        sales.push(doc.get("name"));
+      }
     });
+    console.log(sales);
+    //send items before error handling
+    res.send(sales);
+  }).catch(function(error){
+    console.log(error);
+  });
+})
+
+app.post('/update_user_info', (req,res)=>{
+
+  firebaseQuery = firebaseFirestore.collection("Customer").doc(req.body.uid);
+
+  if(req.body.firstName != ""){
+    firebaseQuery.update({
+      firstName: req.body.firstName
+    })
+  }
+
+  if(req.body.lastName != ""){
+    firebaseQuery.update({
+      lastName: req.body.lastName
+    })
+  }
+
+  if(req.body.phone != ""){
+    firebaseQuery.update({
+      phone: req.body.phone
+    })
+  }
+
+  if(req.body.address != ""){
+    firebaseQuery.update({
+      address: req.body.address
+    })
+  }
+
+  res.send("Success")
+
 })
 
 app.get('*', function(req, res){
-    res.redirect('/404.html');
+  res.redirect('/404.html');
 });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}/`); //boots up node.js server
+  console.log(`server started at http://localhost:${port}/`); //boots up node.js server
 });
